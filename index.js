a22e0abd8ead27c5c6d0c0b7c6088bce3c1f0ed1
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 17:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let depthFirstSearch = (startNode, vertices, edges) => {
-  let queue = [startNode]
-  let visitedQueue = [startNode]
+  let queue = [startNode];
+  startNode.discovered = true;
+  let visitedQueue = [startNode];
 
   while(queue.length != 0) {
     let currentNode = queue.pop()
