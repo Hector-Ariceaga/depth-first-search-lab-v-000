d45662077062e7044e9c3e97c09c46765f2c1dac
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 17:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let depthFirstSearch = (startNode, vertices, edges) => {
   let queue = [startNode]
   let visitedQueue = [startNode]
-  
+
   while(queue.length != 0) {
     let currentNode = queue.pop()
     if (!currentNode.discovered) {
